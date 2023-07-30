66745a7e702352667ba7381bcd7e89e1e2d01f11
Chris Whitley <ureyx6@gmail.com>
2023-07-30 13:04:54 -0700

    added basic functions of calculator

---
+++ script.js
@@ -0,0 +1,16 @@
+function addValues (inputOne, inputTwo) {
+ return (inputOne + inputTwo);
+}
+
+function subtractValues (inputOne, inputTwo) {
+    return (inputOne - inputTwo);
+}
+
+function multiplyValues (inputOne, inputTwo) {
+    return (inputOne * inputTwo)
+}
+
+function divideValues (inputOne, inputTwo) {
+    return (inputOne/inputTwo) 
+}
+
