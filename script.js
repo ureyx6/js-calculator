4f0b873b90868636c8f689249dcfb3bf4980055b
Chris Whitley <ureyx6@gmail.com>
2023-07-30 21:55:55 -0700

    added function to add nums to display

--- script.js
+++ script.js
@@ -2,44 +2,49 @@
 const numberButtons = document.querySelectorAll('.btn-number')
 const operatorButtons = document.querySelectorAll('.btn-operator')
 const clearButton = document.querySelector('.btn-clear')
+const display = document.querySelector('.calculator-display')
 let operandOne = null
 let operandTwo = null
 let operator = ""
+let displayData = ""
 //operate function
-function operate (operator, operandOne) {
+function operate(operandOne, operandTwo) {
     if (operator = "+") {
-        return addValues (operandOne, operandTwo);
+        return addValues(operandOne, operandTwo);
     }
     else if (operator = "-") {
-        return subtractValues (operandOne, operandTwo);
+        return subtractValues(operandOne, operandTwo);
     }
     else if (operator = "*") {
-        return divideValues (operandOne, operandTwo);
+        return divideValues(operandOne, operandTwo);
     }
     else if (operator = "/") {
-        return divideValues (operandOne, operandTwo);
+        return divideValues(operandOne, operandTwo);
     }
 }
 
 
 //evaluation functions
-function addValues (inputOne, inputTwo) {
- return (inputOne + inputTwo);
+function addValues(inputOne, inputTwo) {
+    return (inputOne + inputTwo);
 }
 
-function subtractValues (inputOne, inputTwo) {
+function subtractValues(inputOne, inputTwo) {
     return (inputOne - inputTwo);
 }
 
-function multiplyValues (inputOne, inputTwo) {
+function multiplyValues(inputOne, inputTwo) {
     return (inputOne * inputTwo)
 }
 
-function divideValues (inputOne, inputTwo) {
-    return (inputOne/inputTwo) 
+function divideValues(inputOne, inputTwo) {
+    return (inputOne / inputTwo)
 }
 //display function
 numberButtons.forEach(e => {
-    e.addEventListener('click', function () {   
+    e.addEventListener('click', function () {
+        elementText = e.textContent
+        displayData += elementText
+        display.textContent = displayData
     })
 })
