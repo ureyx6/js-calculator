db0bc161fed45b05c2bc2772353dabddcb57cba7
Chris Whitley <ureyx6@gmail.com>
2023-07-30 23:27:33 -0700

    fixed issues within operate function
    that was leading to errors

--- script.js
+++ script.js
@@ -3,23 +3,31 @@ const numberButtons = document.querySelectorAll('.btn-number')
 const operatorButtons = document.querySelectorAll('.btn-operator')
 const clearButton = document.querySelector('.btn-clear')
 const display = document.querySelector('.calculator-display')
+const equalsButton = document.querySelector('.btn-equals')
 let operandOne = null
 let operandTwo = null
 let operator = ""
 let displayData = ""
 //operate function
+function reset() {
+    operandOne = null
+    operandTwo = null
+    operator = ""
+    return;
+}
+
 function operate(operateOne, operateTwo) {
-    if (operator = "+") {
+    if (operator == "+") {
         return addValues(operateOne, operateTwo);
     }
-    else if (operator = "-") {
+    else if (operator == "-") {
         return subtractValues(operateOne, operateTwo);
     }
-    else if (operator = "*") {
-        return divideValues(operateOne, operateTwo);
+    else if (operator == "*") {
+        return multiplyValues(operateOne, operateTwo);
     }
-    else if (operator = "/") {
-        return divideValues(operateOne, operateTwo);
+    else if (operator == "/") {
+        return divideValues(operateOne, operateTwo).toFixed(2);
     }
 }
 
@@ -40,22 +48,42 @@ function multiplyValues(inputOne, inputTwo) {
 function divideValues(inputOne, inputTwo) {
     return (inputOne / inputTwo)
 }
-//display function
+//display functions
 numberButtons.forEach(e => {
     e.addEventListener('click', function () {
-        elementText = e.textContent
-        displayData += elementText
-        display.textContent = displayData
+        if (display.textContent == "+" || display.textContent == "-" || display.textContent == "*" || display.textContent == "/") {
+            display.textContent = ""
+        }
+           
+        display.textContent += e.textContent
+        displayData = display.textContent
     })
 })
 operatorButtons.forEach(e => {
     e.addEventListener('click', function () {
-        elementText = e.textContent
-        displayData += elementText
-        display.textContent = displayData
+        if (display.textContent == "") {
+            display.textContent = "ERROR!"
+            reset();
+            return;
+        }
+        else if (display.textContent != "" && operandOne != null) {
+            operandTwo = display.textContent
+            display.textContent = operate(parseInt(operandOne), parseInt(operandTwo))
+            return;
+
+        } else if (operandOne === null) {
+            operator = e.textContent
+            operandOne = displayData
+            displayData = ""
+            display.textContent = e.textContent;
+            return;
+        }
+        display.textContent += e.textContent
+        displayData = display.textContent
     })
 })
 clearButton.addEventListener('click', function () {
+    reset();
+    display.textContent = ""
     displayData = ""
-    display.textContent = displayData
 })
